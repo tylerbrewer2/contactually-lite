2623fe9c2ba19f77f39be7a2a4ef051780935d75
tylerbrewer2 <tbrewer1@mail.mvnu.edu>
2015-10-27 22:20:07 -0400

    add delete feature

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,9 @@
 class ContactsController < ApplicationController
+
+  def destroy
+    contact = Contact.find(params[:id])
+    user = User.find(contact.user_id)
+    contact.destroy
+    redirect_to user_path(user)
+  end
 end
